c40b5b311dba98375cd99ee4024ed942df2ab2a5
Pen9uin6 <503paulo@gmail.com>
2023-12-29 10:50:47 +0800

    Update IF.py
    fix divide instruction

--- PipelineCPU/IF.py
+++ PipelineCPU/IF.py
@@ -1,15 +1,42 @@
 import re
 from PipelineCPU.pipelineReg import pipelineRegister
 
+def divide_ins(instruction):
+    parts = instruction.split(' ')
+    print(parts)
+    if(parts[0] == 'lw' or parts[0] == 'sw'):
+        opcode = parts[0]
+
+        matches = re.findall(r'\d+', parts[1])
+        num1 = matches[0]
+    
+        matches = re.findall(r'\d+', parts[2])
+        num2 = matches[0]
+        num3 = matches[1]
+
+    elif(parts[0] == 'add' or parts[0] == 'sub'):
+        matches = re.findall(r'\d+', instruction)
+        opcode = parts[0]
+        num1 = matches[0]
+        num2 = matches[1]
+        num3 = matches[2]
+
+    elif(parts[0] == 'beq'):
+        opcode = parts[0]
+        num1, num2, num3 = re.findall(r'-?\d+', instruction)
+    
+    return opcode, num1, num2, num3
+
+
+
 class IF:
     def __init__(self):
         pass
     
     def run(self, instructionMem, pc):
         if len(instructionMem) > pc: #判斷IF要不要作動
-            matches = re.findall(r'\d+', instructionMem[pc])
-            opcode = instructionMem[pc].split()[0]
-            self.IF_ID = pipelineRegister(opcode, int(matches[0]), int(matches[1]), int(matches[2]))
+            code = divide_ins(instructionMem[pc])
+            self.IF_ID = pipelineRegister(code[0], int(code[1]), int(code[2]), int(code[3]))
         else:
             self.IF_ID = None
         
